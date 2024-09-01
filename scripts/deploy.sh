45ba495de33fdb7bcc55a265e7a888d0955afb53
happiestLife <chickenman10@naver.com>
2024-09-01 22:57:06 +0900

    [Feat/CICD] afterInstall 후 실행되는 shell script에서 sudo 권한을 가지고 실행되도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ echo "> StoreManager front deploy success!"
 echo "> Start StoreManager..."
 
 cd store-manager-fe/
-yarn install
-pm2 start --interpreter $(which node) yarn -- start
+sudo yarn install
+sudo pm2 start --interpreter $(which node) yarn -- start
 
-echo "> End StoreManager..."
+echo "> End Startiing StoreManager..."
