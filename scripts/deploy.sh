6914a4f592a3bb4268bb4c2951c45c22ac1e45b7
happiestLife <chickenman10@naver.com>
2024-10-09 14:10:53 +0900

    [Feat/CICD] Codedeploy에서 실행되는 script에서 pm2를 통해 React가 실행되지 않던 버그 fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 echo "> StoreManager front deploy success!"
 echo "> Start StoreManager..."
 
-cd store-manager-fe/
+cd /home/ubuntu/store-manager-fe/
+sudo npm install -g pm2
 sudo yarn install
-sudo pm2 start --interpreter $(which node) yarn -- start
+sudo pm2 start --name store-manager --interpreter $(which node) yarn -- start
 
 echo "> End Startiing StoreManager..."
