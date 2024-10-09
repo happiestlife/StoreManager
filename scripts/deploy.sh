040e63f98aa0d4b6fdef52368a483f4868a1f219
happiestLife <chickenman10@naver.com>
2024-10-09 16:02:51 +0900

    [Feat/CICD] pm2 사용 부분에서 --interpreter 부분은 오류가 발생할 수 있음으로 삭제

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ echo "> Start StoreManager..."
 cd /home/ubuntu/store-manager-fe/
 npm install -g pm2
 yarn install
-pm2 start yarn --name store-manager --interpreter $(which node) --cwd /home/ubuntu/store-manager-fe -- start
+pm2 start yarn --name store-manager --cwd /home/ubuntu/store-manager-fe -- start
 
 echo "> End Startiing StoreManager..."
